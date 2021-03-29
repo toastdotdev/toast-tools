1c1973d3eb37716758435dd28c32f6c3ffa11254
Christopher Biscardi <chris@christopherbiscardi.com>
2021-03-29 16:46:42 -0700

    implement bors

---
+++ packages/mdx/test-loader.mjs
@@ -0,0 +1,11 @@
+import "./test-module-aliases.mjs";
+
+const moduleAliases = {
+  react: "preact/compat",
+  "react-dom": "preact/compat",
+  "react/jsx-runtime": "preact/jsx-runtime",
+};
+
+export const resolve = (specifier, parentModuleURL, defaultResolve) => {
+  return defaultResolve(moduleAliases[specifier] || specifier, parentModuleURL);
+};
